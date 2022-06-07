d0cde012666931a9f11d20d40f6c772009ad3a8d
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-06-07 17:59:54 -0500

    commit tree 2266cd808833742d1d75a4b440cee98285dae28a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+//let newHeader = document.createElement("h1");
+
+//document.body.append(header);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML  = "Christian is the champion"
+
+
+console.log(newHeader);
